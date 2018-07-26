c1bedb1b31844e50620eeccff24c74d91332a8e8
ManKai <1007941428@qq.com>
2018-07-26 16:51:36 +0800

    login coding

--- src/main/java/com/lanou/controller/UserController.java
+++ src/main/java/com/lanou/controller/UserController.java
@@ -3,5 +3,12 @@ package com.lanou.controller;
 /**
  * Created by peng on 2018/7/25.
  */
+
+@Controller
 public class UserController {
+    @RequestMapping(value = "login")
+    public String login(){
+        return "login";
+    }
+
 }
